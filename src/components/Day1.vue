95185899a0e32825de08ab2686ea1efda5765e74
Jeremy Langevin <langevinj832@gmail.com>
2021-06-17 12:24:24 -0400

    Setup template for Day1 component.

---
+++ src/components/Day1.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="frame">
+        <div class="center">
+		    <p>Happy coding :)</p>
+        </div>
+    </div>
+</template>
+
+<style scoped src="../assets/styles/day1.css"></style>
+
+<script>
+
+</script>
