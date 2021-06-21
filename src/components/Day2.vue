9a23f8fc11e5df00274c97238373a15dfdf8bc41
Jeremy Langevin <langevinj832@gmail.com>
2021-06-20 20:25:49 -0400

    Base colors added.

--- src/components/Day2.vue
+++ src/components/Day2.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="circle">
+        <div class="segment-top"></div>
+        <div class="segment-bottom"></div>
     </div>
 </template>
 
@@ -16,5 +18,25 @@
         position: absolute;
         margin-top: -100px;
         margin-left: -90px;
+        overflow: hidden;
+    }
+
+   .segment-top {
+        float: left;
+        width: 100%;
+        height: 300px;
+        background: #7BDBF8;
+        margin-bottom: 0;
+        position: absolute;
+    }
+
+    .segment-bottom {
+        float: left;
+        width: 100%;
+        height: 100px;
+        margin-top: 120px;
+        background: #DFCF6F;
+        z-index: 20;
+        position: absolute;
     }
 </style>
